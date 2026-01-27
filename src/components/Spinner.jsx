2678438eb7d87fc55acb2c1f040faa35f12da223
Akshi Ahuja <akshiahuja2204@gmail.com>
2026-01-27 07:10:49 +0530

    Fetched data concept, added loading spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,19 @@
+import { ClipLoader } from "react-spinners";
+
+const overRide = {
+    display: "block",
+    margin: "100px auto"
+}
+
+const Spinner = ( { loading } ) => {
+  return (
+    <ClipLoader 
+        color="#4338ca"
+        loading= { loading }
+        cssOverride={overRide}
+        size={150}
+    />
+  )
+}
+
+export default Spinner
